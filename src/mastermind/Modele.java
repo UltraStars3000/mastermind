fe7eefcae180513888c6ad00247ef490849a9def
Benjamin Vigroux <bvigroux@pcalb-mm0000.univ-jfc.fr>
2021-02-17 12:16:31 +0100

    Model update

--- src/mastermind/Modele.java
+++ src/mastermind/Modele.java
@@ -1,11 +1,26 @@
 package mastermind;
 
 import java.awt.Color;
+import java.util.Observable;
 
-public class Modele {
+public class Modele extends Observable {
 	
-	Color[] COULEURS = {Color.yellow, Color.green, Color.blue, Color.magenta, Color.red, Color.orange, Color.white, Color.black};
+	static Color[] COULEURS = {Color.yellow, Color.green, Color.blue, Color.magenta, Color.red, Color.orange, Color.white, Color.black};
 	static int N_TENTATIVES = 10;
 	static int DIFFICULTE = 4;
-	public enum Etat{EN_COURS,GAGNE,PERDU};
+	static public enum Etat{EN_COURS,GAGNE,PERDU};
+	
+	Etat etat;
+	Rangee combinaison;
+	Rangee[] propositions = new Rangee[N_TENTATIVES];
+	int tentative;
+	
+	
+	public Modele() {
+		this.etat = Etat.EN_COURS;
+		for(int i=0;i<Modele.DIFFICULTE;i++) {
+			this.combinaison.jetons[i] = Modele.COULEURS[(int)(Math.random()*(Modele.COULEURS.length+1))];
+		}
+		tentative = 0;
+	}
 }
