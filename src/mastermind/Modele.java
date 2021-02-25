d8d6706fb1e88cf13279e90d86a4f6c771354839
UltraStars3000 <youtube.ultra@laposte.net>
2021-02-25 19:06:44 +0100

    Modele update

--- src/mastermind/Modele.java
+++ src/mastermind/Modele.java
@@ -1,6 +1,7 @@
 package mastermind;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Observable;
 
 public class Modele extends Observable {
@@ -11,7 +12,7 @@ public class Modele extends Observable {
 	static public enum Etat{EN_COURS,GAGNE,PERDU};
 	
 	Etat etat;
-	Rangee combinaison;
+	Rangee combinaison = new Rangee();
 	Rangee[] propositions = new Rangee[N_TENTATIVES];
 	int tentative;
 	
@@ -19,8 +20,61 @@ public class Modele extends Observable {
 	public Modele() {
 		this.etat = Etat.EN_COURS;
 		for(int i=0;i<Modele.DIFFICULTE;i++) {
-			this.combinaison.jetons[i] = Modele.COULEURS[(int)(Math.random()*(Modele.COULEURS.length+1))];
+			this.combinaison.jetons[i] = Modele.COULEURS[(int)(Math.random()*(Modele.COULEURS.length))];
 		}
-		tentative = 0;
+		this.tentative = 0;
+	}
+	
+	public void evaluerProposition() {
+		ArrayList<Color> temp_usr = new ArrayList<Color>();
+		ArrayList<Color> temp_sol = new ArrayList<Color>();
+		int temp_noi = 0;
+		int temp_bla = 0;
+		
+		for(int i=0;i<Modele.DIFFICULTE;i++) {
+			temp_usr.add(this.propositions[this.tentative].jetons[i]);
+			temp_sol.add(this.combinaison.jetons[i]);
+		}
+		
+		for(int i=0;i<Modele.DIFFICULTE;i++) {
+			if(temp_usr.get(i) == temp_sol.get(i)) {
+				temp_noi++;
+				temp_usr.set(i, null); //DummyValue
+				temp_sol.set(i, null); //DummyValue
+			}
+			for(int j=0;j<Modele.DIFFICULTE;j++) {
+				if(temp_usr.get(i) == temp_sol.get(j)) {
+					temp_bla++;
+					temp_usr.set(i, null); //DummyValue
+					temp_sol.set(j, null); //DummyValue
+				}
+			}
+		}
+		this.propositions[this.tentative].noirs = temp_noi;
+		this.propositions[this.tentative].blancs = temp_bla;
+		
+		if(temp_noi == DIFFICULTE) {
+			this.etat = Etat.GAGNE;
+		}
+	}
+	
+	public void nouvelleProposition() {
+		if(this.tentative == N_TENTATIVES) {
+			this.etat = Etat.PERDU;
+		}else {
+			this.tentative++;
+		}
+	}
+	
+	public void archiverProposition() {
+		
+	}
+	
+	public void afficherPropositions() {
+		
+	}
+	
+	public void afficherEncours() {
+		
 	}
 }
